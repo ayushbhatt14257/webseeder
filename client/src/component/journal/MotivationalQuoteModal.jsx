259865733484f79a59db8adba497fa31c5e41be9
ayush bhatt <mitul.l.fst@gmail.com>
2025-01-24 17:29:58 +0530

    last commit

--- client/src/component/journal/MotivationalQuoteModal.jsx
+++ client/src/component/journal/MotivationalQuoteModal.jsx
@@ -4,11 +4,14 @@ import "./style.css"
 const MotivationalQuoteModal = ({ quote, closeModal }) => {
   return (
     <div className="modal">
+      <div className="modalContainer">
+
       <blockquote>
         <p>{quote.quote}</p>
         <footer>- {quote.author}</footer>
       </blockquote>
       <button onClick={closeModal}>Close</button>
+    </div>
     </div>
   );
 };
